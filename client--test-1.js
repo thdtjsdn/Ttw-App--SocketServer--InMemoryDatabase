6eba9222f059e32b041a11255635950cbad80ace
thdtjsdn <thdtjsdn@gmail.com>
2024-12-20 02:10:16 +0900

    Create client--test-1.js

---
+++ client--test-1.js
@@ -0,0 +1,43 @@
+import {ServerSocketDatabaseInMemoryClient} from './client/server_socket__database_inmemory_client.js';
+import crypto from 'crypto';
+
+(async function () {
+	const client = new ServerSocketDatabaseInMemoryClient(41000, '127.0.0.1', 'a');
+
+	function getRandomKey() {
+		return crypto.randomBytes(8).toString('hex');
+	}
+
+	function getRandomValue() {
+		return crypto.randomBytes(16).toString('hex');
+	}
+
+	async function setRandomKeyValue() {
+		const key = getRandomKey();
+		const value = getRandomValue();
+		const setResponse = await client.set(key, value);
+		console.log('SET Response:', setResponse); // Output: OK
+		return key;
+	}
+
+	async function getKeyValue(key) {
+		const getResponse = await client.get(key);
+		console.log('GET Response:', getResponse); // Output: value
+	}
+
+	async function getAllKeys() {
+		const keysResponse = await client.keys('*');
+		console.log('KEYS Response:', keysResponse); // Output: list of all keys
+	}
+
+	const iterations = 10000; // Number of iterations for performance test
+
+	console.time('Performance Test');
+
+	for (let i = 0; i < iterations; i++) {
+		const key = await setRandomKeyValue();
+		await getKeyValue(key);
+	}
+
+	console.timeEnd('Performance Test');
+})();
