c250b59205650b4e471653bc66251285a10ac224
thdtjsdn <thdtjsdn@gmail.com>
2024-12-20 22:09:38 +0900

    Update .main.js

--- .main.js
+++ .main.js
@@ -2,7 +2,28 @@ console.log(1);
 
 //----------------------------------------------------------------------------------------------------;
 
+/*/
 // How to use in server mode
+
+const DATABASE_SERVERS = {
+	// PORT, PASSWORD
+	MAIN : [41000, 'a']
+};
+
+for(var s in DATABASE_SERVERS){
+	let PORT = DATABASE_SERVERS[s][0]; // SERVER PORT
+	let PASS = DATABASE_SERVERS[s][1]; // PASSWORD
+
+	DATABASE_SERVERS[s] = global.bootstrap(PORT, PASS);
+}
+
+console.log(DATABASE_SERVERS);
+//*/
+
+//----------------------------------------------------------------------------------------------------;
+
+/*/
+// How to use in servers mode
 
 const DATABASE_SERVERS = {
 	// PORT, PASSWORD
@@ -22,41 +43,85 @@ for(var s in DATABASE_SERVERS){
 }
 
 console.log(DATABASE_SERVERS);
+//*/
+
+//----------------------------------------------------------------------------------------------------;
+
+//*/
+// How to use in servers mode
+
+// 'SET_CB' sample
+
+const DATABASE_SERVERS = {
+	// PORT, PASSWORD
+	MAIN : [41000, 'a']
+
+	, MEMBER : [41010, 'a']
+
+	, STOCK_KR : [41020, 'a']
+	, STOCK_US : [41021, 'a']
+};
+
+function SET_CB(key, value){
+	console.log(1);
+	console.log(Date.now(), key, value);
+
+	// ...
+}
+
+for(var s in DATABASE_SERVERS){
+	let PORT = DATABASE_SERVERS[s][0]; // SERVER PORT
+	let PASS = DATABASE_SERVERS[s][1]; // PASSWORD
+
+	DATABASE_SERVERS[s] = global.bootstrap(PORT, PASS);
+	DATABASE_SERVERS[s].getDB().callbacks.set_cb = SET_CB;
+	console.log(DATABASE_SERVERS[s].getDB().callbacks.set_cb);
+}
+
+//console.log(DATABASE_SERVERS);
+//*/
 
 //----------------------------------------------------------------------------------------------------;
 
 // How to use in component mode
 
 (async function () {
-	const path_backup_data = './.data/41000/database_backup.json.gz';
+	const PORT = 42000;
+	const path_backup_data = './.data/' + PORT + '/database_backup.json.gz';
 	const client = new DatabaseInMemoryClient(path_backup_data);
 
-	try {
-		let response = await client.set('key1', 'value1');
-		console.log('SET Response:', response); // Output: OK
+	// getter;
+	const DB = client.getDB();
+	DB.callbacks.set_cb = function(key, value){
+		console.log('---------- SET_CB ----------')
+		console.log(Date.now(), key, value);
+	};
+	console.log(DB.callbacks.set_cb);
 
-		response = await client.get('key1');
-		console.log('GET Response:', response); // Output: value1
+	// setter;
+	// client.setMemoryStore('key', 'value');
 
-		const keys = await client.keys('*');
-		console.log('KEYS Response:', keys); // Output: [ 'key1' ]
+	let response = client.set('key1', 'value1');
+	console.log('SET Response:', response); // Output: OK
 
-		response = await client.dataBackup();
-		console.log('BACKUP Response:', response); // Output: OK: Backup completed
+	response = client.set_cb('key2', 'value2');
+	console.log('SET_CB Response:', response); // Output: OK
 
-		await client.flushAll();
-		console.log('Data flushed.');
+	response = client.get('key1');
+	console.log('GET Response:', response); // Output: value1
 
-		response = await client.dataRestore();
-		console.log('RESTORE Response:', response); // Output: OK: Restore completed
+	const keys = client.keys('*');
+	console.log('KEYS Response:', keys); // Output: [ 'key1' ]
 
-		response = await client.get('key1');
-		console.log('GET Response after restore:', response); // Output: value1
+	response = await client.dataBackup();
+	console.log('BACKUP Response:', response); // Output: OK: Backup completed
 
-		// getter;
-		const MEMORY_STORE = client.getMemoryStore();
-		// setter;
-		client.setMemoryStore('key', 'value');
-	}
-	catch (err) { console.error('Error:', err); }
+	client.flushAll();
+	console.log('Data flushed.');
+
+	response = await client.dataRestore();
+	console.log('RESTORE Response:', response); // Output: OK: Restore completed
+
+	response = client.get('key1');
+	console.log('GET Response after restore:', response); // Output: value1
 })();
